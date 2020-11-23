df1fee54a56cd71d0fb044566cc7fdd3f63c2e0b
Isaac Michum <isaacmichum@icloud.com>
2020-11-23 13:36:56 -0500

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -2,11 +2,11 @@ def uno():
 
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[13])
 
 if __name__== "__main__":
   uno()
